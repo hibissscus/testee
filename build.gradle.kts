0497b2bf8d375f1da7337353cdb2e5d98d24db8b
Sergei Stepanov <sergei.stepanov@ysura.com>
2020-12-21 22:03:45 +0100

    build.gradle.kts kotlin 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version ("1.4.20")
+    id("org.jetbrains.kotlin.jvm") version ("1.4.21")
     id("maven-publish")
 }
 

048833ecacdd5e450f95df6e7fed32f52aa8cc4f
Sergei Stepanov <sergei.stepanov@ysura.com>
2021-09-11 23:07:54 +0200

    MatryoshkaTest.kt

--- src/test/kotlin/testee/it/tests/example/matryoshka/MatryoshkaTest.kt
+++ src/test/kotlin/testee/it/tests/example/matryoshka/MatryoshkaTest.kt
@@ -1,13 +1,14 @@
 package testee.it.tests.example.matryoshka
 
 import org.testng.annotations.Test
+import testee.it.e2e.core.browser.Browser
 import testee.it.e2e.core.test.TestBase
 import testee.it.e2e.example.BasePage.Companion.open
 import testee.it.e2e.example.matryoshka.Emoji
 import testee.it.e2e.example.matryoshka.MatryoshkaPage
 
 
-class MatryoshkaTest : TestBase(url = "https://pixlr.com/x/") {
+class MatryoshkaTest : TestBase(url = "https://pixlr.com/x/", browser = Browser.CHROME) {
 
     private val imageUrl = "https://i.ibb.co/3p6XkbM/matryoshka.png"
 

94f67bee3c480f3b2d070ee591db3eed95a28968
Sergei Stepanov <sergei.stepanov@ysura.com>
2021-09-28 21:49:57 +0200

    Changes in version 1.4.5 ------------------------ * reportng 1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "it.testee"
-version = "1.4.4"
+version = "1.4.5"
 
 repositories {
     mavenCentral()
@@ -27,7 +27,7 @@ dependencies {
     // testng
     implementation("org.testng", "testng", "7.4.0")
     // reportng
-    implementation("com.github.hibissscus:reportng:1.4.1")
+    implementation("com.github.hibissscus:reportng:1.4.2")
 }
 
 // Customise the "compileKotlin" task.

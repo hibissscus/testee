62b92fcbb5a702f6d2580ae098fea13f424df471
hibissscus <hibissscus@gmail.com>
2022-03-29 12:14:41 +0200

    Changes in version 1.5.3 ------------------------ * selenium-java 4.1.3 * selenium docker 4.1.3-20220327

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "it.testee"
-version = "1.5.2"
+version = "1.5.3"
 
 repositories {
     mavenCentral()
@@ -20,7 +20,7 @@ dependencies {
     // kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.20-M1")
     // selenium
-    implementation("org.seleniumhq.selenium:selenium-java:4.1.2")
+    implementation("org.seleniumhq.selenium:selenium-java:4.1.3")
     // testng
     implementation("org.testng", "testng", "7.5")
     // reportng

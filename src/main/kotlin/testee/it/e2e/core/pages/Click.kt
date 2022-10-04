4b240b3bd4103cfc8a3c96e4af878c186d43f9e4
hibissscus <hibissscus@gmail.com>
2022-10-04 13:15:02 +0200

    Changes in version 1.6.5
    ------------------------
    * kotlin 1.7.20
    * selenium java 4.5.0
    * selenium docker 4.5.0-20220929
    * reportng 1.4.9

--- src/main/kotlin/testee/it/e2e/core/pages/Click.kt
+++ src/main/kotlin/testee/it/e2e/core/pages/Click.kt
@@ -8,30 +8,46 @@ interface Click : Driver, Visibility, Checks {
 
     /**
      *  Check if specific element [WebElement] can be clicked on the [Page]
+     *
+     *  @param refreshed wrapper for a condition, which allows for elements to update by redrawing.
+     *  This works around the problem of conditions which have two parts: find an element and then check for some condition on it.
+     *  For these conditions it is possible that an element is located and then subsequently it is redrawn on the client.
+     *  When this happens a StaleElementReferenceException is thrown when the second part of the condition is checked.
      */
-    fun clickable(element: WebElement): WebElement {
-        return wait().until(ExpectedConditions.elementToBeClickable(element))
+    fun clickable(element: WebElement, refreshed: Boolean = false): WebElement {
+        return wait().until(
+            if (refreshed) ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(element))
+            else ExpectedConditions.elementToBeClickable(element)
+        )
     }
 
     /**
      *  Check if specific element by [By] can be clicked on the [Page]
+     *
+     *  @param refreshed wrapper for a condition, which allows for elements to update by redrawing.
+     *  This works around the problem of conditions which have two parts: find an element and then check for some condition on it.
+     *  For these conditions it is possible that an element is located and then subsequently it is redrawn on the client.
+     *  When this happens a StaleElementReferenceException is thrown when the second part of the condition is checked.
      */
-    fun clickable(by: By): WebElement {
-        return wait().until(ExpectedConditions.elementToBeClickable(by))
+    fun clickable(by: By, refreshed: Boolean = false): WebElement {
+        return wait().until(
+            if (refreshed) ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(by))
+            else ExpectedConditions.elementToBeClickable(by)
+        )
     }
 
     /**
      *  Click on specific element [WebElement] on the [Page]
      */
-    fun click(element: WebElement) {
-        clickable(element).click()
+    fun click(element: WebElement, refreshed: Boolean = false) {
+        clickable(element, refreshed).click()
     }
 
     /**
      *  Click on specific element [By] on the [Page]
      */
-    fun click(by: By) {
-        clickable(by).click()
+    fun click(by: By, refreshed: Boolean = false) {
+        clickable(by, refreshed).click()
     }
 
     /**
